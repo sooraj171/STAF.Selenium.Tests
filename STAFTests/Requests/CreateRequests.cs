27b7fb9b707c2656f2c82b310700997e727e2395
Sooraj <sooraj171@outlook.com>
2025-12-29 21:47:30 -0500

    Added latest 4.39 package for selenium and STAF.UI.API 4.3.2

--- STAFTests/Requests/CreateRequests.cs
+++ STAFTests/Requests/CreateRequests.cs
@@ -11,7 +11,7 @@ namespace STAFTests
         public ListOfUsersDTO GetUsers()
         {
             RestClient restClient = new RestClient(strClient);
-            RestRequest restRequest = new RestRequest("/api/users?page=2",Method.Get);
+            RestRequest restRequest = new RestRequest("/api/users",Method.Get);
             restRequest.AddHeader("Accept", "application/json");
             restRequest.RequestFormat = DataFormat.Json;
 
@@ -22,93 +22,5 @@ namespace STAFTests
 
             return Users;
         }
-
-
-        /*public string getExcelData(string strExpJsonColName)
-        {
-            Excel.Application xlApp;
-
-            Excel.Workbook xlWorkBook;
-
-            Excel.Worksheet xlWorkSheet;
-
-            Excel.Range range;
-
-
-
-            string str = "";
-
-            int rCnt;
-
-            int cCnt;
-
-            int rw = 0;
-
-            int cl = 0;
-
-
-
-            xlApp = new Excel.Application();
-
-            xlWorkBook = xlApp.Workbooks.Open(@"C:\Working\RNDProjects\APITestProject\DataLib\TestData\ExpectedJson.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
-
-            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-
-
-
-            range = xlWorkSheet.UsedRange;
-
-            rw = range.Rows.Count;
-
-            cl = range.Columns.Count;
-
-
-
-
-
-            for (rCnt = 1; rCnt <= rw; rCnt++)
-
-            {
-
-                //for (cCnt = 1; cCnt <= cl; cCnt++)
-
-                //{
-
-                str = (string)(range.Cells[rCnt, 1] as Excel.Range).Value2;
-
-
-
-                if (str.ToLower().Trim() == strExpJsonColName.ToLower().Trim())
-
-                {
-
-                    str = (string)(range.Cells[rCnt, 2] as Excel.Range).Value2;
-
-                    return str;
-
-                }
-
-                //}
-
-            }
-
-
-
-            xlWorkBook.Close(true, null, null);
-
-            xlApp.Quit();
-
-
-
-            _ = Marshal.ReleaseComObject(xlWorkSheet);
-
-            _ = Marshal.ReleaseComObject(xlWorkBook);
-
-            _ = Marshal.ReleaseComObject(xlApp);
-
-            return str;
-
-        }*/
-
     }
 }
